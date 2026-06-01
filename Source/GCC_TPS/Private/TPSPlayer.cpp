40a1267d2d11fc4b287f2856bc6c79abfffa39c1
iamfreakin <qjaqjaqja1877@gmail.com>
2026-06-01 12:50:03 +0900

    feat: 3인칭 카메라 구현을 위한 SpringArm 컴포넌트 설정

---
+++ Source/GCC_TPS/Private/TPSPlayer.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GCC_TPS/Public/TPSPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+ATPSPlayer::ATPSPlayer()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// TPS 카메라를 SpringArm 컴포넌트에 부착
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->SetRelativeLocation(FVector(0.0f, 70.0f, 90.0f));
+	SpringArm->TargetArmLength = 400.0f;
+
+}
+
+// Called when the game starts or when spawned
+void ATPSPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATPSPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/GCC_TPS/Public/TPSPlayer.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TPSPlayer.generated.h"
+
+UCLASS()
+class GCC_TPS_API ATPSPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATPSPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// 스프링 암 컴포넌트 생성
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* SpringArm;
+};
