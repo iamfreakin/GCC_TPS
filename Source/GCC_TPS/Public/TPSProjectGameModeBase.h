64b758910e20028db2b118434a410ab530aed65e
iamfreakin <qjaqjaqja1877@gmail.com>
2026-06-01 11:50:23 +0900

    feat: GameModeBase 뼈대 생성 및 BP 생성

---
+++ Source/GCC_TPS/Public/TPSProjectGameModeBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GCC_TPS_API ATPSProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+};
