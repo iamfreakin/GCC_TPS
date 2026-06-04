01d8606df2b840278e637a69b2abcddbd354feae
iamfreakin <qjaqjaqja1877@gmail.com>
2026-06-04 16:29:31 +0900

    feat: Sniper - 피격 물리 충격 적용(AddForceAtLocation)

--- Source/GCC_TPS/Public/TPSPlayer.h
+++ Source/GCC_TPS/Public/TPSPlayer.h
@@ -97,6 +97,10 @@ public:
 	UPROPERTY(EditDefaultsOnly, Category = SniperUI)
 	TSubclassOf<class UUserWidget> sniperUIFactory;
 	
+	// 스나이퍼 사격 명중 시 표시할 총알 파편 효과 선언 
+	UPROPERTY(EditDefaultsOnly, Category = BulletEffect)
+	class UNiagaraSystem* bulletEffectFactory;
+	
 	// 이동 방향
 	FVector direction;
 	
@@ -124,7 +128,4 @@ public:
 	// 스나이퍼 줌 입력 함수 선언
 	void SniperZoom();
 	
-	// 스나이퍼 사격 명중 시 표시할 총알 파편 효과 선언 
-	UPROPERTY(EditDefaultsOnly, Category = BulletEffect)
-	class UNiagaraSystem* bulletEffectFactory;
 };
