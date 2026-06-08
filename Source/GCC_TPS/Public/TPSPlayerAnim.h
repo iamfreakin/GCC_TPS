098a7a51d09fd9aa11c1809611e5bb2f3422636e
iamfreakin <qjaqjaqja1877@gmail.com>
2026-06-08 11:32:26 +0900

    feat: TPSPlayerAnim 애니메이션 인스턴스 구성 (Speed 변수, NativeUpdateAnimation 을 통한 속도 반영)

---
+++ Source/GCC_TPS/Public/TPSPlayerAnim.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TPSPlayerAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GCC_TPS_API UTPSPlayerAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	//매 프레임 자동 호출 - 소유 폰의 상태를 읽어서 변수 갱신(Tick)
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+    
+	//캐릭터의 현재 이동 속도(ABP State Machine의 전이 조건에서 사용)
+	UPROPERTY(BlueprintReadOnly, Category = PlayerAnim)
+	float speed = 0.f;
+};
